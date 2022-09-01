0933289ffef7cdce4234a0973b110ee5c8002330
PPPARK <sdc00035@naver.com>
2022-09-01 15:18:37 +0900

    '0901'

--- project_1/main/urls.py
+++ project_1/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name="main")
+    path('', views.main, name="main"),
+    path('database/', views.database, name='database'),
+    path('database/<int:id>/', views.detail, name = 'detail'),
+    path('delete/<int:id>', views.delete, name = 'delete'),
 ]
--- project_1/main/views.py
+++ project_1/main/views.py
@@ -1,9 +1,36 @@
 from multiprocessing import context
-from django.shortcuts import render
+import re
+from django.shortcuts import render, get_object_or_404, redirect
+
+from pages import models
 
 # Create your views here.
 def main(request):
     context ={
-        "alert" : False
+        "alert" : False,
+        "wrong" : False
     }
     return render(request, 'main.html', context)
+
+# 로그인 했을 때 게시글 전체 확인하기
+def database(request):
+    articles = models.login.objects.all() 
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'database.html', context)
+
+# 게시글 상세보기
+def detail(request, id):
+    # filter와 get의 차이를 명확하게 알아야할듯
+    article = models.login.objects.get(id = id)
+    context = {
+        "article" : article
+    }
+    return render(request, 'detail.html', context)
+
+def delete(request,id):
+    article = models.login.objects.get(id = id)
+    article.delete()
+    return render(request, 'database.html')
